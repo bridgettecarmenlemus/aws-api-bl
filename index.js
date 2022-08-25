01574ea373f943e7170d4688f5c05729de886050
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2022-08-25 08:54:36 -0400

    retry api

---
+++ index.js
@@ -0,0 +1,14 @@
+import  express from "express";
+import cors from 'cors'
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT= 4040
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)
+})
+
